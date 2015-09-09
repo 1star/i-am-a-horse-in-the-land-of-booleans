a130b5305699edf0bf573ac0983b5615fbdcc512
Pete Pajuheimo <paju@Petes-MacBook-Air.local>
2015-09-09 16:17:47 +0300

    Tehtava 2 palautus

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -2,27 +2,36 @@
   (:refer-clojure :exclude [boolean]))
 
 (defn boolean [x]
-  ":(")
+  (if x true false))
 
 (defn abs [x]
-  ":(")
+  (if (< x 0) (- x) x))
 
 (defn divides? [divisor n]
-  ":(")
+  (if (=(mod n divisor) 0) true false))
 
 (defn fizzbuzz [n]
-  ":(")
+	(cond
+		(=(mod n 15) 0) "gotcha!"
+		(=(mod n 3) 0) "fizz"
+		(=(mod n 5) 0) "buzz"
+		:else ""))
 
 (defn teen? [age]
-  ":(")
+  (and (>= age 13) (<= age 19)))
 
 (defn not-teen? [age]
-  ":(")
+	(if (and (>= age 13) (<= age 19)) false true))
 
 (defn generic-doublificate [x]
-  ":(")
+  (cond
+  	(number? x) (* 2 x)
+  	(empty? x) nil
+  	(list? x) (* 2 (count x))
+  	(vector? x) (* 2 (count x))
+  	:else true))
 
-(defn leap-year? [year]
-  ":(")
+ (defn leap-year? [year] 
+ 	(and (zero? (mod year 4)) (or (not= (mod year 100) 0) (zero? (mod year 400)))))
 
 ; '_______'
